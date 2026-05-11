b10843dbd3950a6c47b29df2a47d9b3085c66c22
Valli-Veeravel <valliveeravel12@gmail.com>
2026-05-10 19:10:54 -0700

    Rename reservation manager test

--- src/test/java/test/ReservationManagerTest.java
+++ src/test/java/test/ReservationManagerTest.java
@@ -17,7 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class CoreModelSmokeTest {
+class ReservationManagerTest {
     @Test
     void availabilityScheduleReservesAndReleasesCapacity() {
         AvailabilitySchedule schedule = new AvailabilitySchedule("schedule-1");
